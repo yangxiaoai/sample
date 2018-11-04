efc130a8c39f734dec9981ae76ed95d47f655144
yiyaojun <574821703@qq.com>
2018-11-04 14:50:18 +0000

    Finish user signup

--- config/app.php
+++ config/app.php
@@ -75,10 +75,10 @@ return [
     | The application locale determines the default locale that will be used
     | by the translation service provider. You are free to set this value
     | to any of the locales which will be supported by the application.
-    |
+    | en
     */
 
-    'locale' => 'en',
+    'locale' => 'zh-CN',
 
     /*
     |--------------------------------------------------------------------------
