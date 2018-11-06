835a2bf137625d66e383319baf6a9dd8a2dd242a
yiyaojun <574821703@qq.com>
2018-11-06 10:03:58 +0000

    Finish user curd

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' => ['show','create','store','index']
+        ]);
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
     //
     public function create()
     {
@@ -36,5 +46,52 @@ class UsersController extends Controller
     	Auth::login($user);
     	session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 	    return redirect()->route('users.show',[$user]);
+    }
+
+    public function edit(User $user)
+    {
+        try{
+            $this->authorize('update',$user);
+    	    return view('users.edit',compact('user'));
+        }catch(AuthorizationException $exception){
+            return abort(403,$exception->getMessage());
+        }
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update',$user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password); 
+        }
+
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show',$user->id);
+    }
+
+    //首页
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
+    //shanchu
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
